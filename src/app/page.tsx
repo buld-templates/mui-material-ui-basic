9d0b9592b607a77302418649a4dc1dd7f436facc
HugoB2409 <46574029+HugoB2409@users.noreply.github.com>
2023-12-26 18:49:31 -0500

    import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,37 @@
-import {
-    Box,
-    Button,
-    Typography,
-    Checkbox,
-    Switch,
-    TextField,
-    Rating,
-    Slider,
-    Fab,
-    Avatar,
-    Chip,
-    Divider,
-    Container,
-    Paper,
-    Stack,
-    Link,
-    Accordion,
-    AccordionSummary,
-    AccordionDetails,
-    AppBar,
-    Toolbar,
-    Breadcrumbs,
-    Card,
-    CardActions,
-    CardMedia,
-    CardContent,
-    Table,
-    TableCell,
-    TableBody,
-    TableHead,
-    TableRow,
-    List,
-    ListItem,
-    ListItemText,
-} from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Checkbox from "@mui/material/Checkbox";
+import Switch from "@mui/material/Switch";
+import TextField from "@mui/material/TextField";
+import Rating from "@mui/material/Rating";
+import Slider from "@mui/material/Slider";
+import Fab from "@mui/material/Fab";
+import Avatar from "@mui/material/Avatar";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import Link from "@mui/material/Link";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Table from "@mui/material/Table";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 import Grid from "@mui/material/Unstable_Grid2";
 import Image from "next/image";
 import NextLink from "next/link";
